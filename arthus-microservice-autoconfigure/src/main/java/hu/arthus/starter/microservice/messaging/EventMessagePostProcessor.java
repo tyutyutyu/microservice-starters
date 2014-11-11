6d89968638a5543ade8633d3357a4ff22c686e01
István Földházi <istvan.foldhazi@gmail.com>
2014-11-11 14:08:52 +0100

    Nem használt osztály törlése

--- arthus-microservice-autoconfigure/src/main/java/hu/arthus/starter/microservice/messaging/EventMessagePostProcessor.java
+++
@@ -1,9 +0,0 @@
-package hu.arthus.starter.microservice.messaging;
-
-import org.springframework.aop.support.NameMatchMethodPointcutAdvisor;
-
-public class EventMessagePostProcessor extends NameMatchMethodPointcutAdvisor {
-
-	private static final long serialVersionUID = 1L;
-
-}
