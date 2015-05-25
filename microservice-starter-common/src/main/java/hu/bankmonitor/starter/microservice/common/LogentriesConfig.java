b1f76c059a5ac318118ad01fb2c53bcdee23cc07
István Földházi <istvan.foldhazi@gmail.com>
2015-05-25 17:11:22 +0200

    Add Logentries config

---
+++ microservice-starter-common/src/main/java/hu/bankmonitor/starter/microservice/common/LogentriesConfig.java
@@ -0,0 +1,45 @@
+package hu.bankmonitor.starter.microservice.common;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.PatternLayout;
+
+import com.logentries.logback.LogentriesAppender;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogentriesConfig {
+
+	@Value("${application.logEntriesToken:null}")
+	private String token;
+
+	@PostConstruct
+	public void init() {
+
+		if (token != null) {
+			LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+			LogentriesAppender logentriesAppender = new LogentriesAppender();
+			logentriesAppender.setContext(loggerContext);
+			logentriesAppender.setDebug(false);
+			logentriesAppender.setToken(token);
+			logentriesAppender.setSsl(false);
+			logentriesAppender.setFacility("USER");
+			PatternLayout patternLayout = new PatternLayout();
+			patternLayout.setContext(loggerContext);
+			patternLayout.setPattern("%d{HH:mm:ss.SSS} [%thread] [%-5level] %logger{50} - %msg%n");
+			logentriesAppender.setLayout(patternLayout);
+
+			logentriesAppender.start();
+
+			Logger root = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+			root.addAppender(logentriesAppender);
+		}
+	}
+
+}
