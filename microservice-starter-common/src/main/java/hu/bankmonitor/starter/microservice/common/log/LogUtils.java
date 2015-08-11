06b83087997411c1bb664e6569a9c3e51a33bd06
István Földházi <istvan.foldhazi@gmail.com>
2015-08-11 13:37:41 +0200

    Downgrade Java version to 1.7

--- microservice-starter-common/src/main/java/hu/bankmonitor/starter/microservice/common/log/LogUtils.java
+++ microservice-starter-common/src/main/java/hu/bankmonitor/starter/microservice/common/log/LogUtils.java
@@ -2,6 +2,7 @@ package hu.bankmonitor.starter.microservice.common.log;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,15 +15,19 @@ public final class LogUtils {
 
 		StringBuffer sb = new StringBuffer();
 		sb.append("\nURI        :").append(request.getRequestURI());
-		Collections.list(request.getHeaderNames()).stream().forEach(n -> {
-			sb.append("\nHeader     :").append(n).append("=").append(request.getHeader(n));
-		});
-		request.getParameterMap().entrySet().stream().forEach(e -> {
-			sb.append("\nParam      :").append(e.getKey()).append("=").append(Arrays.toString(e.getValue()));
-		});
+		for (Object n : Collections.list(request.getHeaderNames())) {
+			sb.append("\nHeader     :").append(n).append("=").append(request.getHeader(n.toString()));
+		}
+		for (Object e : request.getParameterMap().entrySet()) {
+			Map.Entry<?, ?> entry = (Map.Entry<?, ?>) e;
+			if (entry.getValue() instanceof String[]) {
+				sb.append("\nParam      :").append(entry.getKey()).append("=").append(Arrays.toString((String[]) entry.getValue()));
+			} else if (entry.getValue() instanceof String) {
+				sb.append("\nParam      :").append(entry.getKey()).append("=").append((String) entry.getValue());
+			}
+		}
 
 		return sb.toString();
-
 	}
 
 }
