402faca1884f9ccb714824bd528126d58cacfcc7
István Földházi <istvan.foldhazi@gmail.com>
2015-10-06 13:07:20 +0200

    Remove unused codes

--- microservice-autoconfigure/src/main/java/hu/bankmonitor/starter/microservice/messaging/EventClassFinderConfiguration.java
+++ microservice-autoconfigure/src/main/java/hu/bankmonitor/starter/microservice/messaging/EventClassFinderConfiguration.java
@@ -1,30 +1,23 @@
 package hu.bankmonitor.starter.microservice.messaging;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.regex.Pattern;
 
-import lombok.extern.slf4j.Slf4j;
-
-import org.axonframework.eventhandling.annotation.EventHandler;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.type.filter.AssignableTypeFilter;
-import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
 public class EventClassFinderConfiguration {
-
-	private static final Pattern AXON_STARTER_PACKAGE_PATTERN = Pattern.compile("hu\\.bankmonitor\\.starter\\.microservice\\.axon\\..*");
 
 	private static final String BASE_PACKAGE = "hu";
 
@@ -58,34 +51,10 @@ public class EventClassFinderConfiguration {
 		}
 	}
 
-	@ConditionalOnClass(value = EventHandler.class)
-	@Configuration
-	public static class AxonEventClassFinderConfiguration {
-
-		@Bean
-		@SuppressWarnings("static-method")
-		public EventClassFinder eventClassFinder() {
-
-			return new AxonEventClassFinder();
-		}
-
-		public static class AxonEventClassFinder implements EventClassFinder {
-
-			@Override
-			@SuppressWarnings("synthetic-access")
-			public Set<String> findEventClasses() {
-
-				return findParameterTypeOfMethodsWithAnnotation(EventHandler.class);
-			}
-
-		}
-	}
-
 	private static Set<String> findParameterTypeOfMethodsWithName(String methodName) {
 
 		ClassPathScanningCandidateComponentProvider componentProvider = new ClassPathScanningCandidateComponentProvider(false);
 		componentProvider.addIncludeFilter(new AssignableTypeFilter(AbstractMessageHandler.class));
-		componentProvider.addExcludeFilter(new RegexPatternTypeFilter(AXON_STARTER_PACKAGE_PATTERN));
 
 		Set<String> eventClasses = new HashSet<>();
 		for (BeanDefinition bd : componentProvider.findCandidateComponents(BASE_PACKAGE)) {
@@ -96,40 +65,6 @@ public class EventClassFinderConfiguration {
 
 				for (Method method : allDeclaredMethods) {
 					if (methodName.equals(method.getName())) {
-
-						assert method.getParameterCount() != 1;
-
-						String eventClass = method.getParameters()[0].getType().getName();
-						log.debug("Handled event class: {}", eventClass);
-						eventClasses.add(eventClass);
-					}
-				}
-
-			} catch (IllegalArgumentException | ClassNotFoundException | LinkageError e) {
-				// TODO: FI: review exception handling
-				log.error("Class load error: {}", e.getMessage());
-				log.error("Exception:", e);
-			}
-		}
-
-		return eventClasses;
-	}
-
-	private static Set<String> findParameterTypeOfMethodsWithAnnotation(Class<? extends Annotation> annotationType) {
-
-		ClassPathScanningCandidateComponentProvider componentProvider = new ClassPathScanningCandidateComponentProvider(false);
-		componentProvider.addIncludeFilter(new AssignableTypeFilter(Object.class));
-		componentProvider.addExcludeFilter(new RegexPatternTypeFilter(AXON_STARTER_PACKAGE_PATTERN));
-
-		Set<String> eventClasses = new HashSet<>();
-		for (BeanDefinition bd : componentProvider.findCandidateComponents(BASE_PACKAGE)) {
-
-			try {
-				Method[] allDeclaredMethods =
-						ReflectionUtils.getAllDeclaredMethods(ClassUtils.forName(bd.getBeanClassName(), EventClassFinderConfiguration.class.getClassLoader()));
-
-				for (Method method : allDeclaredMethods) {
-					if (method.getAnnotation(annotationType) != null) {
 
 						assert method.getParameterCount() != 1;
 
