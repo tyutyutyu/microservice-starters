ba6f8c3396f63c158413a35a813edd5762a69998
István Földházi <istvan.foldhazi@gmail.com>
2016-01-18 13:05:17 +0100

    Enable ErrorControllerAdvice by default

---
+++ microservice-starter-common/src/main/java/hu/bankmonitor/starter/microservice/common/ErroHandlingAutoConfiguration.java
@@ -0,0 +1,17 @@
+package hu.bankmonitor.starter.microservice.common;
+
+import hu.bankmonitor.starter.microservice.common.errorhandling.ErrorControllerAdvice;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ErroHandlingAutoConfiguration {
+
+	@Bean
+	@SuppressWarnings("static-method")
+	ErrorControllerAdvice errorControllerAdvice() {
+
+		return new ErrorControllerAdvice();
+	}
+
+}
