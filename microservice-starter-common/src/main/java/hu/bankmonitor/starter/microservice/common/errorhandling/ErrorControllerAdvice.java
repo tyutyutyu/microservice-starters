b9c48cf79a2c28e16d95e5647a958d16bc07e7b5
István Földházi <istvan.foldhazi@gmail.com>
2016-09-27 11:54:59 +0200

    Maven dependencies upgrade, fix ResourceNotFoundException

--- microservice-starter-common/src/main/java/hu/bankmonitor/starter/microservice/common/errorhandling/ErrorControllerAdvice.java
+++ microservice-starter-common/src/main/java/hu/bankmonitor/starter/microservice/common/errorhandling/ErrorControllerAdvice.java
@@ -32,6 +32,17 @@ public class ErrorControllerAdvice {
 		return RestErrorMessage.create(exception);
 	}
 
+	@ExceptionHandler(ResourceNotFoundException.class)
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	@SuppressWarnings("static-method")
+	public RestErrorMessage handleResourceNotFoundException(HttpServletRequest request, ResourceNotFoundException exception) {
+
+		LogUtils.logException(exception, request);
+
+		return RestErrorMessage.create(exception);
+	}
+
 	@ExceptionHandler(MicroserviceStarterRuntimeException.class)
 	@ResponseBody
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
--- microservice-starter-common/src/main/java/hu/bankmonitor/starter/microservice/common/errorhandling/ResourceNotFoundException.java
+++ microservice-starter-common/src/main/java/hu/bankmonitor/starter/microservice/common/errorhandling/ResourceNotFoundException.java
@@ -3,37 +3,13 @@ package hu.bankmonitor.starter.microservice.common.errorhandling;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- *
- * @author nnonn
- */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends MicroserviceStarterRuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public ResourceNotFoundException() {
-		super();
-	}
-
-	@SuppressWarnings("ucd")
-	public ResourceNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	@SuppressWarnings("ucd")
-	public ResourceNotFoundException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	@SuppressWarnings("ucd")
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-
-	@SuppressWarnings("ucd")
-	public ResourceNotFoundException(Throwable cause) {
-		super(cause);
+	public ResourceNotFoundException(ExceptionData exceptionData) {
+		super(exceptionData);
 	}
 
 }
